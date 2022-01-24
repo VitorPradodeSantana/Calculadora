ef0218018d8d88a9da92f153e7aef229eae2fc64
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-01-24 11:43:20 -0300

    Terminado todo o estilo da calculadora e comecando o javascript

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let limpar = document.querySelector('.clear');
+
+function inserir(num){
+    let resultado = document.querySelector('#resultado').value = num;
+    return num;
+}
+
+limpar.addEventListener('click', function(){
+    let resultado = document.getElementById('resultado').value = '';
+});
+
+function calcular(n1, n2){
+    let operacao = `${n1} ${operador} ${n2}`; 
+}
+
+
+
+
