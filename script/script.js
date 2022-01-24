b12620b50641269703fa60a5b4b2ce8d73c1031e
VitorPradodeSantana <vitor_shotokan@hotmail.com>
2022-01-24 16:49:09 -0300

    Terminando o script da calculadora

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 let limpar = document.querySelector('.clear');
 
 function inserir(num){
-    let resultado = document.querySelector('#resultado').value = num;
-    return num;
+    let resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado + num;
+
 }
 
 limpar.addEventListener('click', function(){
@@ -13,6 +14,19 @@ function calcular(n1, n2){
     let operacao = `${n1} ${operador} ${n2}`; 
 }
 
+function limparUm(){
+    let resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular(){
+    let resultado = document.getElementById('resultado').value;
+    if(resultado){
+        document.getElementById('resultado').value = eval(resultado);
+    }else{
+       let resultado = document.getElementById('resultado').value = "Digite uma operação !"; 
+    }
+}
 
 
 
